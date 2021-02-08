124fefa50e2a22e2be68d2864273c488d55eec9d
erik <kalybekov.erlan@gmail.com>
2021-02-08 18:38:14 +0600

    Simple TreeSet demo

---
+++ src/com/erik/Main.java
@@ -0,0 +1,14 @@
+package com.erik;
+import java.util.TreeSet;
+public class Main {
+
+    public static void main(String[] args) {
+        TreeSet<String> letters=new TreeSet<String>();
+        letters.add("E");
+        letters.add("d");
+        letters.add("c");
+        letters.add("B");
+        letters.add("A");
+        System.out.println("TreeSet : "+letters);
+    }
+}
